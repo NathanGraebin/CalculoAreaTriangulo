85e7fa9ee84360dc67c92410ce5d983fb2c1640c
Gabriella <gabriella.galvao@edu.unipar.br>
2023-03-02 20:54:22 -0300

    1 # 2 3 4

--- src/main/java/com/mycompany/mavenproject1/AreaTriangulo2.java
+++ src/main/java/com/mycompany/mavenproject1/AreaTriangulo2.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.mavenproject1;
 
 import java.util.Scanner;
@@ -15,28 +14,32 @@ public class AreaTriangulo2 {
 
     public static void main(String[] args) {
         Scanner Ler = new Scanner(System.in);
-         
+
         double alturaCentimetros;
         double larguraCentimetros;
-      
-        
+
         System.out.println("Qual é Altura em centrimetros");
         alturaCentimetros = Ler.nextFloat();
-        
+
         System.out.println("Qual é Largura em centrimetros");
         larguraCentimetros = Ler.nextFloat();
-        
-        double areaEmCentimetros = ((alturaCentimetros*larguraCentimetros)/2);
-       
-        
+
+        double areaEmCentimetros = ((alturaCentimetros * larguraCentimetros) / 2);
+
         double areaConvertidaEmMetros = areaEmCentimetros / 100;
-        
+
         System.out.println("a area do triangulo é de : " + areaEmCentimetros + " cm ");
-        
+
         System.out.println("a area do triangulo é de : " + areaConvertidaEmMetros + " metros ");
-         
-        
-        
-        
+
+        int numeroConveridoParaInt = (int) areaEmCentimetros;
+        System.out.println("CONVERTIDO PARA INT " + numeroConveridoParaInt);
+
+        if (areaEmCentimetros > 10.0) {
+            System.out.println("A area é MAIOR que 10");
+        } else {
+            System.out.println("A area é MENOR que 10");
+        }
+
     }
 }
